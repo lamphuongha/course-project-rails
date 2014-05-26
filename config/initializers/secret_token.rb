a1f1a845616c3f045ec26eb1f9bf686f1cb4f58c
ha.lam.phuong <ha.lam.phuong@evolable.com>
2014-05-26 11:05:23 +0900

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseProjectRails::Application.config.secret_token = '2d4ad9109e34da4b70c7cf795d699f549444d7bc17ae53822ebd6a71e0d92c458c7673e8d7f17933ee2cf6913d6da84735d9054fa10ae395986112aae0408c85'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CourseProjectRails::Application.config.session_store :cookie_store, key: '_course-project-rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CourseProjectRails::Application.config.session_store :active_record_store
